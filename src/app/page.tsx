aba82aa8a37f8092f3f166467a182c6cee67ff90
Avijit Biswas <avijit@devdynamics.ai>
2024-08-30 21:32:03 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { FaRegSmile } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div>
+      Hello app!
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile size={20} />}
+      >
+        Click Me
+      </Button>
+    </div>
+  );
+}
