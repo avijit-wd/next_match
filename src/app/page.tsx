73dced72b2ec72b5be500078654c63580dbe04c5
Avijit Biswas <avijit@devdynamics.ai>
2024-09-22 11:52:26 +0530

    loading error dynamic routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
+import { auth, signOut } from "@/auth";
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
-import { FaRegSmile } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
       Hello app!
-      <Button
-        as={Link}
-        href="/members"
-        color="primary"
-        variant="bordered"
-        startContent={<FaRegSmile size={20} />}
-      >
-        Click Me
-      </Button>
+      <h3 className="text-2xl font-semibold">User session data: </h3>
+      {session ? (
+        <div>
+          <pre>{JSON.stringify(session)}</pre>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <Button color="primary" variant="bordered" type="submit">
+              Sign out
+            </Button>
+          </form>
+        </div>
+      ) : (
+        <div>Not signed in</div>
+      )}
     </div>
   );
 }
---
+++ src/lib/schemas/loginSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email({
+    message: "Invalid Email address",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
