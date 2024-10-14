328d71b9b0a7458e5fec187bec79beea9aa8bf7d
Avijit Biswas <avijit@devdynamics.ai>
2024-10-14 23:39:26 +0530

    added latest

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+import LikeButton from "@/components/LikeButton";
 import { calculateAge } from "@/lib/utils";
 import { Card, CardFooter, Image } from "@nextui-org/react";
 import { Member } from "@prisma/client";
@@ -6,9 +8,16 @@ import React from "react";
 
 type Props = {
   member: Member;
+  likedIds: string[];
 };
 
-export default function MemberCard({ member }: Props) {
+export default function MemberCard({ member, likedIds }: Props) {
+  const hasLiked = likedIds.includes(member.userId);
+
+  const preventLinkAction = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
   return (
     <Card as={Link} href={`/members/${member.userId}`} isPressable fullWidth>
       <Image
@@ -18,7 +27,11 @@ export default function MemberCard({ member }: Props) {
         src={member.image || "/images/user.png"}
         className="aspect-square object-cover"
       />
-
+      <div onClick={preventLinkAction}>
+        <div className="absolute top-3 right-3 z-50">
+          <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+        </div>
+      </div>
       <CardFooter className="justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient">
         <div className="flex flex-col text-white">
           <span className="font-semibold">
